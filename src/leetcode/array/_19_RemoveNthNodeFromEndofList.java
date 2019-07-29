10489bd09bc8de4eecbb3344e6a4144309981644
zhouxinghang <13633843273@163.com>
2019-07-29 15:34:33 +0800

    删除链表倒数第n个节点

--- src/leetcode/array/_19_RemoveNthNodeFromEndofList.java
+++ src/leetcode/array/_19_RemoveNthNodeFromEndofList.java
@@ -8,8 +8,27 @@ package leetcode.array;
  * After removing the second node from the end, the linked list becomes 1->2->3->5.
  */
 public class _19_RemoveNthNodeFromEndofList {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
 
+    /**
+     * 采用两个指针，间隔 n+1 步，后面的节点的下一个节点就是要删除的节点
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode preHead = new ListNode(0);
+        preHead.next = head;
+        ListNode left = preHead, right = preHead;
+        for (int i = 0; i < n+1; i++) {
+            right = right.next;
+        }
+        while (right != null) {
+            left = left.next;
+            right = right.next;
+        }
+        left.next = left.next.next;
+        // 有可能删除的是第一个节点
+        return preHead.next;
     }
 }
 
---
+++ src/leetcode/array/_23_MergekSortedLists.java
@@ -0,0 +1,28 @@
+package leetcode.array;
+
+/**
+ * @author zhouxinghang
+ * @date 2019-07-29
+ * 合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
+ *
+ * Input:
+ * [
+ *   1->4->5,
+ *   1->3->4,
+ *   2->6
+ * ]
+ * Output: 1->1->2->3->4->4->5->6
+ */
+public class _23_MergekSortedLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+
+    }
+
+    class ListNode {
+        int val;
+        leetcode.array.ListNode next;
+        ListNode(int x) { val = x; }
+    }
+}
+
+
