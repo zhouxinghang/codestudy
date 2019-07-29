ce50461c686bd020f88b76df9671c8972d70c569
zhouxinghang <13633843273@163.com>
2019-07-29 15:54:27 +0800

    合并多个有序链表，采用分治合并的方法

--- src/leetcode/array/_23_MergekSortedLists.java
+++ src/leetcode/array/_23_MergekSortedLists.java
@@ -14,13 +14,50 @@ package leetcode.array;
  * Output: 1->1->2->3->4->4->5->6
  */
 public class _23_MergekSortedLists {
-    public ListNode mergeKLists(ListNode[] lists) {
 
+    /**
+     * 1：第一个想到的方法，依次比较每个链表的头结点，取最小的那一个
+     * 2：分治，两两合并
+     * 这里采用第二种方法
+     * @param lists
+     * @return
+     */
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return doMerge(lists, 0, lists.length - 1);
+    }
+
+    private ListNode doMerge(ListNode[] lists, int left, int right) {
+        if (left == right) {
+            return lists[left];
+        }
+        int mid = (left + right) / 2;
+        ListNode l1 = doMerge(lists, left, mid);
+        ListNode l2 = doMerge(lists, mid+1, right);
+        return mergeTwoList(l1, l2);
+    }
+
+    private ListNode mergeTwoList(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoList(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoList(l1, l2.next);
+            return l2;
+        }
     }
 
     class ListNode {
         int val;
-        leetcode.array.ListNode next;
+        ListNode next;
         ListNode(int x) { val = x; }
     }
 }
