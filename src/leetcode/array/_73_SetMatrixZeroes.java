d7a7bee38a59a34e9b752bfc4fe2ec6022d97f68
zhouxinghang <13633843273@163.com>
2019-11-17 20:58:56 +0800

    矩阵置零

--- src/leetcode/array/_73_SetMatrixZeroes.java
+++ src/leetcode/array/_73_SetMatrixZeroes.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
  * @date 2019-08-27
  * 矩阵置零
  * 给定一个 m x n 的矩阵，如果一个元素为 0，则将其所在行和列的所有元素都设为 0。请使用原地算法。
+ * 空间复杂度 O(1)
  * Input:
  * [
  *   [0,1,2,0],
@@ -21,4 +22,41 @@ import java.util.ArrayList;
  * ]
  */
 public class _73_SetMatrixZeroes {
+
+    /**
+     * 暴力枚举，先遍历一遍，标记哪些位置需要修改为 0，然后再遍历一遍，将需要修改为 0 的修改为 0。
+     * 需要第二次才能修改为 0，是为了防止干扰
+     * @param matrix
+     */
+    public void setZeroes(int[][] matrix) {
+        int MODIFIED = -1000000;
+        int R = matrix.length;
+        int C = matrix[0].length;
+
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                if (matrix[r][c] == 0) {
+                    for (int k = 0; k < C; k++) {
+                        if (matrix[r][k] != 0) {
+                            matrix[r][k] = MODIFIED;
+                        }
+                    }
+                    for (int k = 0; k < R; k++) {
+                        if (matrix[k][c] != 0) {
+                            matrix[k][c] = MODIFIED;
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                // Make a second pass and change all MODIFIED elements to 0 """
+                if (matrix[r][c] == MODIFIED) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+    }
 }
